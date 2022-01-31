e2bc566ec3019e844a9071e8ff7a6dd3dd4c0397
yuxiRen <rita.ren.yuxi@gmail.com>
2022-01-31 15:27:35 -0800

    add Snake class

---
+++ src/moduls/Snake.ts
@@ -0,0 +1,26 @@
+class Snake {
+  head: HTMLElement;
+  bodies: HTMLCollection;
+  element: HTMLElement;
+  constructor() {
+    this.element = document.getElementById("snake")!;
+    this.head = document.querySelector("#snake > div") as HTMLElement;
+    this.bodies = this.element.getElementsByTagName("div");
+  }
+  get X() {
+    return this.head.offsetLeft;
+  }
+  get Y() {
+    return this.head.offsetTop;
+  }
+  set X(value: number) {
+    this.head.style.left = value + "px";
+  }
+  set Y(value: number) {
+    this.head.style.top = value + "px";
+  }
+  increaseBody() {
+    this.element.insertAdjacentHTML("beforeend", "<div></div>");
+  }
+}
+export default Snake;
