04fbad60672e59f71323e08917df9a1eb21fd6bf
yuxiRen <rita.ren.yuxi@gmail.com>
2022-02-01 12:17:35 -0800

    snake's run()

--- src/moduls/GameControl.ts
+++ src/moduls/GameControl.ts
@@ -5,7 +5,8 @@ class GameControl {
   snake: Snake;
   food: Food;
   scorePanel: ScorePanel;
-  direction: string = "";
+  direction = "ArrowRight";
+  isLive = true;
   constructor() {
     this.food = new Food();
     this.snake = new Snake();
@@ -14,9 +15,32 @@ class GameControl {
   }
   init() {
     document.addEventListener("keydown", this.keyDownHandler.bind(this));
+    this.run();
   }
   keyDownHandler(event: KeyboardEvent) {
     this.direction = event.key;
+  }
+  run() {
+    let X = this.snake.X;
+    let Y = this.snake.Y;
+    switch (this.direction) {
+      case "ArrowUp":
+        Y -= 10;
+        break;
+      case "ArrowDown":
+        Y += 10;
+        break;
+      case "ArrowLeft":
+        X -= 10;
+        break;
+      case "ArrowRight":
+        X += 10;
+        break;
+    }
+    this.snake.X = X;
+    this.snake.Y = Y;
+    this.isLive &&
+      setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
   }
 }
 export default GameControl;
