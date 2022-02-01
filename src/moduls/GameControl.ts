03658686f020f8916902eb098bdf666439d05c25
yuxiRen <rita.ren.yuxi@gmail.com>
2022-02-01 11:15:45 -0800

    listen to the keyboard event

--- src/moduls/GameControl.ts
+++ src/moduls/GameControl.ts
@@ -5,12 +5,18 @@ class GameControl {
   snake: Snake;
   food: Food;
   scorePanel: ScorePanel;
+  direction: string = "";
   constructor() {
     this.food = new Food();
     this.snake = new Snake();
     this.scorePanel = new ScorePanel();
     this.init();
   }
-  init() {}
+  init() {
+    document.addEventListener("keydown", this.keyDownHandler.bind(this));
+  }
+  keyDownHandler(event: KeyboardEvent) {
+    this.direction = event.key;
+  }
 }
 export default GameControl;
